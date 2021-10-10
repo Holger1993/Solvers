8e201703cff545d60f3821f4f6a6a6e3714f1d95
Holger Lindström <holgerlindstrom93@gmail.com>
2021-10-10 21:46:59 +0200

    Started working on the kakuro solver

--- KakuroSolver/kakurosolver.py
+++ KakuroSolver/kakurosolver.py
@@ -1,7 +1,11 @@
 
 
-
-
-
-def solve():
-    pass
+def solve(acr, down):
+    emptyKakuro = acr
+    for i in range(len(acr)):
+        for j in range(len(acr[0])):
+            if acr[i][j] == '0' and down[i][j] == '0':
+                emptyKakuro[i][j] = 0
+            else:
+                emptyKakuro[i][j] = '.'
+    return emptyKakuro
--- KakuroSolver/main.py
+++ KakuroSolver/main.py
@@ -1,7 +1,28 @@
 import kakurosolver as ks
 
 def main():
-    pass
+    with open('Kakuros/kakuro1.txt') as f:
+        lines = f.read().splitlines()
+    f.close()
+    across = []
+    down = []
+    acr = True
+    for line in lines:
+        if line == '':
+            acr = False
+            continue
+        lin = line.split(',')
+        row = []
+        for l in lin:
+            row.append(l)
+        if acr:
+            across.append(row)
+        else:
+            down.append(row)
+    
+    kakuro = ks.solve(across, down)
+    for row in kakuro:
+        print(row)
 
 if __name__ == '__main__':
     main()
